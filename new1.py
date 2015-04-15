4854e62c04a33ebc3fca91b6ed618e3dd1e79305
Sumanthch <ch.sumanth096@gmail.com>
2015-04-15 18:28:02 +0530

    Delete new1.py

--- new1.py
+++
@@ -1,8 +0,0 @@
-import urllib
-urls = ["http://www.nytimes.com","http://www.CNN.com"]
-i=0
-while i < len(urls):
-	htmlfile = urllib.urlopen(urls[i])
-	htmltext = htmlfile.read()
-	print htmltext()
-	i=i+1
